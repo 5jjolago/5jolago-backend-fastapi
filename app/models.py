135489dbdd835dbbadb8eecccae2cfebe5529c38
yeonchulkim <yeonchul0807@nate.com>
2024-02-22 20:37:46 +0900

    test'

--- app/models.py
+++ app/models.py
@@ -7,10 +7,10 @@ Base = declarative_base()
 class Bookmark(Base):
     __tablename__ = "bookmarks"
 
-    neighborhood = Column(String, nullable=False)
-    user_name = Column(String, nullable=False)
+    neighborhood = Column(String(50), nullable=False)
+    user_name = Column(String(50), nullable=False)
     age = Column(Integer, nullable=False)
-    gender = Column(String, nullable=False)
+    gender = Column(String(10), nullable=False)
 
     __table_args__ = (
         PrimaryKeyConstraint('neighborhood', 'user_name'),
