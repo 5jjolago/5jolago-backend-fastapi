831b4a806915824545cf9f8664e2fa00c6bac5fd
yeonchulkim <yeonchul0807@nate.com>
2024-02-23 17:57:44 +0900

    setseetst

--- app/main.py
+++ app/main.py
@@ -202,7 +202,7 @@ async def create_bookmark(
     return new_bookmark
 
 # 사용자 탈퇴로 인한 북마크 삭제하기 
-@app.delete("/bookmarks/", dependencies=[Depends(jwt_bearer)],response_model=List[schemas.Bookmark])
+@app.delete("/bookmarks/", dependencies=[Depends(jwt_bearer)],response_model=schemas.Bookmark)
 async def delete_bookmark_endpoint(user_name: str = Depends(get_current_user), db: Session = Depends(get_db)):
     """
     주어진 북마크 ID에 해당하는 북마크를 삭제하는 엔드포인트.
@@ -222,7 +222,7 @@ async def delete_bookmark_endpoint(user_name: str = Depends(get_current_user), d
     return {"message": "Bookmark deleted successfully"}
 
 # 북마크 삭제하기  
-@app.delete("/bookmarks/{neighborhood}/", dependencies=[Depends(jwt_bearer)],response_model=List[schemas.Bookmark])
+@app.delete("/bookmarks/{neighborhood}/", dependencies=[Depends(jwt_bearer)],response_model=schemas.Bookmark)
 async def delete_bookmark_endpoint( neighborhood: str,user_name: str = Depends(get_current_user), db: Session = Depends(get_db)):
     """
     주어진 북마크 ID에 해당하는 북마크를 삭제하는 엔드포인트.
