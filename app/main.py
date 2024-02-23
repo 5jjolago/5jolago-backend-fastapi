8dfe66d0352db5e27466ab57a66f09fa1e37a4d8
yeonchulkim <yeonchul0807@nate.com>
2024-02-23 15:57:57 +0900

    estset

--- app/main.py
+++ app/main.py
@@ -17,18 +17,18 @@ from sqlalchemy.exc import IntegrityError
 app = FastAPI()
 
 # cors 설정
-origins = [
-    "http://127.0.0.1:3000",
-    "http://localhost:3000"
-]
+# origins = [
+#     "http://127.0.0.1:3000",
+#     "http://localhost:3000"
+# ]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 
 JWK = Dict[str, str]
