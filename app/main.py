63260ca900f4dd182a72a37c6bdde03008be01d5
yeonchulkim <yeonchul0807@nate.com>
2024-03-02 22:39:02 +0900

    cors setting

--- app/main.py
+++ app/main.py
@@ -40,19 +40,19 @@ async def startup_event():
 
 
 
-# cors 설정
-# origins = [
-#     "http://127.0.0.1:3000",
-#     "http://localhost:3000"
-# ]
+cors 설정
+origins = [
+    "http://127.0.0.1:3000",
+    "http://localhost:3000"
+]
 
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=origins,
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 JWK = Dict[str, str]
