3cea84ff18da6558d3da56689227664ce5a297dd
yeonchulkim <yeonchul0807@nate.com>
2024-03-04 16:16:18 +0900

    encoding test

--- app/main.py
+++ app/main.py
@@ -180,6 +180,8 @@ async def create_bookmark(
     새 북마크를 생성하는 엔드포인트.
     """
     # 새로운 북마크를 생성하여 데이터베이스에 추가
+    print("#############################")
+    print(neighborhood)
     new_bookmark = models.Bookmark(neighborhood=neighborhood, user_name=user_name, age=age, gender=gender)
     db.add(new_bookmark)
 
