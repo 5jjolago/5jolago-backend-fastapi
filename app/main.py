5b9e71f487468090fce859b3c34c099c6aba784e
yeonchulkim <yeonchul0807@nate.com>
2024-03-07 15:54:31 +0900

    dfdfdf

--- app/main.py
+++ app/main.py
@@ -53,7 +53,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["POST", "GET", "OPTIONS"],
+    allow_methods=["POST", "GET", "OPTIONS", "DELETE"],
     allow_headers=["*"],
 )
 
